874c76fa71418ac368f3c0b8266b2d701a14cc8c
Gabriel Ramos <gaboramos241@gmail.com>
2022-07-29 17:40:16 -0400

    generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const finish = [".com", ".net"];
+
+  function randomSelector() {
+    return Math.floor(Math.random() * 2 - 1 + 1);
+  }
+
+  function domainGenerator(pronoun, adj, noun, finish) {
+    const resultString = ``;
+    resultString = resultString.concat(pronoun[randomSelector()]);
+    resultString = resultString.concat(adj[randomSelector()]);
+    resultString = resultString.concat(noun[randomSelector()]);
+    resultString = resultString.concat(finish[randomSelector()]);
+    return resultString;
+  }
+
+  console.log(domainGenerator(pronoun, adj, noun, finish));
 };
